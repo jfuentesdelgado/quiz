44d76ec7220a75dec73426f6f74c649fe5547e1d
jose_jfuentes <jfuentes.jfd@gmail.com>
2015-07-31 01:01:00 +0200

    delete pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -84,3 +84,10 @@ exports.update = function(req, res) {
     req.quiz.save( {fields: ["pregunta", "respuesta"]}).then(function(){ res.redirect('/quizes');});
   }
 };
+
+// DELETE/quizes/:id
+exports.destroy = function (req, res) {
+  req.quiz.destroy().then(function(){
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
