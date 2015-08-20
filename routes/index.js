d2ce73f43bc45c0b2e1fdba5d1ca129a84fe4c57
jose_jfuentes <jfuentes.jfd@gmail.com>
2015-08-20 15:54:52 +0200

    moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -10,28 +10,31 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz' , errors: []});
 });
 
-//Autoload de peticiones con :quizId
-router.param('quizId', quizController.load);
+//Autoloads de peticiones con parametro :
+router.param('quizId', quizController.load); //autoload :quizId 
+router.param('commentId', commentController.load); //autoload :commentId
 
 //Rutas de session
 router.get('/login', sessionController.new); //formulario login
 router.post('/login', sessionController.create); //crear sesion
 router.delete('/login', sessionController.destroy); //destruir sesión
 
-
+// Rutas quizes
 router.get('/quizes',                       quizController.index);
 router.get('/quizes/:quizId(\\d+)',         quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',  quizController.answer);
-router.get('/quizes/new',                   session.loginRequired, quizController.new);
-router.post('/quizes/create',               session.loginRequired, quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',    session.loginRequired, quizController.edit);
-router.put('/quizes/:quizId(\\d+)',         session.loginRequired, quizController.update);
-router.delete('/quizes/:quizId(\\d+)',      session.loginRequired, quizController.destroy);
+router.get('/quizes/new',                   sessionController.loginRequired, quizController.new);
+router.post('/quizes/create',               sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 
+// Rutas commentes
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
-
+// Rutas /author
 router.get('/author', function(req, res, next){
   res.render('author', {name: 'Jose Fuentes Delgado', imagen: 'CV2015.jpg', link: 'http://jfuentes.es', errors: []});
 });
